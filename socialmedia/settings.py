12720772682cb4b39397297c683b7082838bdbe5
adhi85 <adimuthanikatt@gmail.com>
2022-12-17 16:56:56 +0530

    final

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -81,35 +81,30 @@ WSGI_APPLICATION = 'socialmedia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'kQQ7RyG4YXSa2eXxlkVI',
+#         'HOST': 'containers-us-west-165.railway.app',
+#         'PORT': 5608 
 #     }
 # }
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'kQQ7RyG4YXSa2eXxlkVI',
-        'HOST': 'containers-us-west-165.railway.app',
-        'PORT': 5608 
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  
+        "PORT": 5432,  
     }
 }
-
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "postgres",
-#         "PASSWORD": "postgres",
-#         "HOST": "db",  
-#         "PORT": 5432,  
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
